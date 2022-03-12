eb3298fd4b3090a3105ae64b820684b616f9b50c
DiegoKld <diegokld23@gmail.com>
2022-03-12 18:04:01 -0500

    Initial Commit

---
+++ dfgsdgf/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dfgsdgf
+//
+//  Created by Diego Fernando Calderon Silva on 12/03/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
